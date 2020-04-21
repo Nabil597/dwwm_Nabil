50ef423b84a40168d0ca7050b997c0404087add4
nab <nab@MacBook-Pro-de-rym.local>
2020-04-21 13:24:00 +0200

    v3

--- couleurs/couleur.js
+++ couleurs/couleur.js
@@ -1,1 +1,59 @@
-var body = document.getElementById("body");
+function createCookie(name, value, days) {
+    // permet de créer un cookie
+    if (days) {
+        // si le nombre de jour est renseigné, on le transforme en millieme de seconde
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    //le cookie doit contenir clé=valeur;expires=temps;path=nomDomaine
+    document.cookie = name + "=" + value + "; " + expires + "; path=/";
+}
+
+function readCookie(name) {
+    // on récupère tous les cookies du site en une fois, séparé par ;
+    // on range dans un tableau chaque cookie
+    var listeCookies = document.cookie.split('; ');
+    for (let i = 0; i < listeCookies.length; i++) {
+        // pour chaque cookie, on sépare en tableau la clé et la valeur
+        var unCookie = listeCookies[i].split("=");
+        // si la clé correspond au cookie cherché, on renvoi la valeur
+        if (unCookie[0] == name) return unCookie[1];
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    // pour supprimer un cookie, on le périme
+    createCookie(name, "", -1);
+}
+
+// recup des variables
+var body = document.getElementsByTagName("body")[0];
+
+
+// function qui change la couleur de body lors du click
+    var btnred = document.getElementsByClassName("btnred")[0];
+    var btngreen = document.getElementsByClassName("btngreen")[0];
+    var btnblue = document.getElementsByClassName("btnblue")[0];
+
+function changeColor(color){
+    
+    switch (color){
+        case "red":
+            body.style.backgroundColor = "red";
+        break;
+
+        case "green":
+            body.style.backgroundColor = "green";
+        break;
+
+        case "blue":
+            body.style.backgroundColor = "lightblue";
+        break;
+    }
+}
+document.getElementsByClassName("btnred")[0].addEventListener("click",function(){changeColor("red");});
+document.getElementsByClassName("btngreen")[0].addEventListener("click",function(){changeColor("green");});
+document.getElementsByClassName("btnblue")[0].addEventListener("click",function(){changeColor("blue");});
---
+++ nbVisite/script.js
@@ -0,0 +1,55 @@
+var paragraphe = document.getElementById("texte");
+var btnReinitialiser = document.getElementById("btnReinitialiser");
+
+
+ btnReinitialiser.addEventListener("click", reinitialiser);
+
+valeur=readCookie("nbVisite");
+if (valeur == null) {
+    createCookie("nbVisite", 1, 3);
+    valeur=1;
+}
+else {
+    valeur++;
+    createCookie("nbVisite",valeur, 3);
+    
+}
+
+paragraphe.innerHTML = "Nombre de visite: " + valeur;
+
+
+function reinitialiser() {
+    createCookie("nbVisite", 0, 3);
+    paragraphe.innerHTML = "Nombre de visite: 0"; 
+}
+
+function createCookie(name,value,days) {
+    // permet de créer un cookie
+	if (days) {
+        // si le nombre de jour est renseigné, on le transforme en millieme de seconde
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "expires="+date.toGMTString();
+	}
+    else var expires = "";
+    //le cookie doit contenir  clé=valeur;expires=temps;path=nomDomaine
+	document.cookie = name+"="+value+"; " + expires+"; path=/";
+}
+
+function readCookie(name) {
+    // on récupère tous les cookies du site en une fois, séparé par ; 
+    // on range dans un tableau chaque cookie
+    var listeCookies = document.cookie.split('; ');
+	for(let i=0;i < listeCookies.length;i++) {
+        // pour chaque cookie, on sépare en tableau la clé et la valeur
+        var unCookie = listeCookies[i].split("=");
+        // si la clé correspond au cookie cherché, on renvoi la valeur
+		if (unCookie[0]==name) return unCookie[1];
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+    // pour supprimer un cookie, on le périme
+	createCookie(name,"",-1);
+}
