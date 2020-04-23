90e8471cac1c778c03d5e897bf9ee19385e4b643
nab <nab@MacBook-Pro-de-rym.local>
2020-04-23 10:29:44 +0200

    eval2

---
+++ EVAL_JS_Nabil/taux.js
@@ -0,0 +1,49 @@
+var capital = document.getElementById("capital");
+capital.addEventListener("input", verifEntier);
+
+var taux = document.getElementById("taux");
+taux.addEventListener("input", verifTaux);
+
+var duree = document.getElementById("duree");
+duree.addEventListener("input", verifEntier);
+
+var mensualite = document.getElementById("mensualite");
+var cout = document.getElementById("cout");
+
+capital.addEventListener("input", function() { calcul (capital.value, taux.value, duree.value) } );
+taux.addEventListener("input", function() { calcul (capital.value, taux.value, duree.value) } );
+duree.addEventListener("input", function() { calcul (capital.value, taux.value, duree.value) } );
+
+function verifEntier(event) {
+
+    var myInput = event.target;
+    var message = (myInput.parentNode).getElementsByClassName("message")[0];
+
+    if (!Number.isInteger(parseFloat(myInput.value))) 
+    {       
+        message.innerHTML = "format incorrect";
+        message.style.color = "red";
+    }
+    else {
+        message.innerHTML = "";
+    }
+}
+
+function verifTaux(event) {
+
+    var myInput = event.target;
+    var message = (myInput.parentNode).getElementsByClassName("message")[0];
+
+    if (parseFloat(myInput.value) > 100) {
+        message.innerHTML = "format incorrect";
+        message.style.color = "red";
+    }
+    else {
+        message.innerHTML = "";
+    }
+}
+
+function calcul (capital, taux, duree) {
+    mensualite.value = parseInt((parseInt(capital) * parseFloat(taux/12)) / (1 - Math.pow(1 + parseFloat(taux/12), - (parseInt(duree) * 12))));
+    cout.value = parseInt(mensualite.value * (parseInt(duree) * 12));
+}
